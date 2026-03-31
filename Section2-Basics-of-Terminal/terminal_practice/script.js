0775f65f4243439525bfb8cc5b986bd1554f3670
Ayush Thakur <ayushtkaur030466@gmail.com>
2026-03-31 10:51:17 +0530

    Section-2 Basics of Terminal

---
+++ Section2-Basics-of-Terminal/terminal_practice/script.js
@@ -0,0 +1,11 @@
+console.log('Hii There');
+
+// \[\]\[\033]0;$TITLEPREFIX:$PWD\007\]\n\[\033[32m\]\u@\h \[\033[35m\]$MSYSTEM \[\033[33m\]\w\[\033[36m\]`__git_ps1`\[\033[0m\]\n$ \[\]
+
+// PS1=\[\]\[\033]0;$TITLEPREFIX:$PWD\007\]\n\[\033[32m\]\u@\h \[\033[35m\]$MSYSTEM \[\033[33m\]\w\[\033[36m\]`__git_ps1`\[\033[0m\]\n$ \[\]
+
+// cd	Change Directory	Moves you between folders/directories in the file system.
+// ls	List	Shows you all the files and folders inside your current directory.
+// cp	Copy	Duplicates a file or folder from one location to another.
+// mv	Move	Moves a file or folder to a new location, OR renames a file/folder in the same location.
+// rm	Remove	Deletes a file or folder permanently.
---
+++ Section2-Basics-of-Terminal/terminal_practice/test.sh
@@ -0,0 +1,9 @@
+num1=5
+num2=5
+echo $((num1 + num2))
+echo "program End"
+
+# to execute the file we have to write it like ./test.sh
+# Create 10 files named app1.js to app10.js To remove all files starting with app and ending with .js rm app*.js , or just change touch with rm.
+
+# for i in {1..10}; do touch "app$i.js"; done
